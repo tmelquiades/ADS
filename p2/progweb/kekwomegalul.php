0c5f91cbc758f628f3d6f22a4270b700c3294580
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2023-08-21 20:47:43 -0300

    introaphp

---
+++ p2/progweb/kekwomegalul.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<b>batman rules !! <3</b>"; //echo imprime uma ou mais variaveis no console
+    $texto = "tenho soninho"; 
+    echo $texto;
+?>
+//tem que abrir o xampp e startar o apache, dps ir no navegador, colar o endereço do codigo e substituir c://xampp/pasta por localhost e dá bom
