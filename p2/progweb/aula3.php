721211ddb19534189447f079c0d5a697185fe342
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2023-08-28 21:05:01 -0300

    2808

---
+++ p2/progweb/aula3.php
@@ -0,0 +1,23 @@
+<?php
+    $texto = "batman";
+    $num = 5;
+    
+    echo "concatenação de strings:<br>";
+    echo "o melhor heroi da DC é o " . $texto;
+    echo "<br>já existiram $num robins<br>";
+    echo $texto.=$texto;
+    echo "<br>";
+    $concatenado = 2;
+    $concatenado.= 5;
+    echo $concatenado;
+
+    echo "<br><br><br>";
+
+    echo "aprendendo a diferença de aspas simples e aspas duplas em php<br>(com exemplos):<br>";
+    echo "[duplas, usando as variaveis] $texto tem mais de $num filmes<br>";
+    echo '[simples, usando as variaveis] $texto tem mais de $num filmes<br>';
+
+    echo "<br><br><br>";
+
+      
+?>
