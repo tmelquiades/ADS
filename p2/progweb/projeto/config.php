86a86720c70bb69b0512ac72db85e30c521dc532
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2023-11-25 12:56:48 -0300

    del projeto

--- p2/progweb/projeto/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'agenda');
-
-    $conn = new MySQLi(HOST, USER, PASS, BASE);
-    
